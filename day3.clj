e74ed3e418f97738b1bbbf92dc4ef83cd0629432
Janos Erdos <erdosj@gmail.com>
2019-12-03 06:49:34 +0100

    Update day3.clj

--- day3.clj
+++ day3.clj
@@ -19,21 +19,15 @@
               [0 0]
               (mapcat (fn [[dir length]] (repeat length dir)) path)))
 
-(defn size [path]
-  (assert (vector? path))
-  [(reduce + (map second (filter (comp #{:right} first) path)))
-   (reduce + (map second (filter (comp #{:down} first) path)))])
-
-(defn man-len [[x y]] (+ (Math/abs x) (Math/abs y)))
+(defn taxi [[x y]]
+  (+ (Math/abs x) (Math/abs y)))
 
 (defn solve [s1 s2]
   (let [p1 (parse s1)
         p2 (parse s2)
         c1 (set (coords p1))
         c2 (set (coords p2))]
-    (apply min (map  man-len (clojure.set/difference
-                              (clojure.set/intersection c1 c2)
-                              #{[0 0]})))))
+    (apply min (map taxi (disj (clojure.set/intersection c1 c2) [0 0])))))
 
 (let [[in1 in2] (clojure.string/split-lines (slurp "/home/erdos/input3.txt"))]
   {:day3-first (solve in1 in2)})
@@ -43,7 +37,7 @@
 (defn path-intersections [path1 path2]
     (let [c1 (set (coords path1))
           c2 (set (coords path2))]
-      (clojure.set/difference (clojure.set/intersection c1 c2) #{[0 0]})))
+      (disj (clojure.set/intersection c1 c2) [0 0])))
 
 (defn coords-steps [path] (mapv vector (coords path) (range)))
 
@@ -51,7 +45,8 @@
       s1 (coords-steps p1)
       s2 (coords-steps p2)]
   {:day3-second
-   (apply min  (for [ci (path-intersections p1 p2)]
-                 (let [f (comp (partial some second)
-                               (partial filter (comp #{ci} first)))]
-                   (+ (f s1) (f s2)))))})
+   (apply min
+          (for [ci (path-intersections p1 p2)
+                :let [f (comp (partial some second)
+                              (partial filter (comp #{ci} first)))]]
+            (+ (f s1) (f s2))))})
