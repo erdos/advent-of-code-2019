3943a78e3b791853ab3fb68e77b85ec9dab10419
Janos Erdos <erdosj@gmail.com>
2019-12-04 06:22:00 +0100

    Update day4.clj

--- day4.clj
+++ day4.clj
@@ -1,29 +1,17 @@
-(ns day4)
 
 (def input [124075 580769])
 
-(def candidates
-  (doall (for [i (range (first input) (second input))
-               :let [s (str i)]
-               :when (or (.contains s "00")
-                         (.contains s "11")
-                         (.contains s "22")
-                         (.contains s "33")
-                         (.contains s "44")
-                         (.contains s "55")
-                         (.contains s "66")
-                         (.contains s "77")
-                         (.contains s "88")
-                         (.contains s "99"))
-               :when (apply <= (map int s))]
-           i)))
+;; first star
+(->> (range (first input) (second input))
+     (map str candidates)
+     (map (partial partition-by int))
+     (filter (partial some (comp #{2 3 4 5 6} count)))
+     (count))
 
-(->> candidates
-     (count)
-     (println "First solution:"))
 
-(->> (map str candidates)
+;; second star
+(->> (range (first input) (second input))
+     (map str candidates)
      (map (partial partition-by int))
      (filter (partial some (comp #{2} count)))
-     (count)
-     (println "Second solution: "))
+     (count))
